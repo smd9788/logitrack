745104b7eeae3b37990e2922d2186733a9746277
Stephen DiCicco <smd09876@gmail.com>
2019-11-03 14:57:57 -0500

    Create customers serializers, api, urls files

---
+++ shipment_manager/customers/api.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets, permissions
+from customers.models import Customer
+from .serializers import CustomerSerializer
+
+# Lead Viewset
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = CustomerSerializer
