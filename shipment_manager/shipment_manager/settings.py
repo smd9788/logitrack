6ac7997339834cf2128e0654cb1b5dd58e7202ce
smd9788 <smd09876@gmail.com>
2019-11-14 19:08:20 -0500

    Add config for react client rendering   - Define view index for client by rendering client/index.html   - Define url route in client/urls.py   - Add client to apps in settings.py   - Include client url into shipment_manager urls.py

--- shipment_manager/shipment_manager/settings.py
+++ shipment_manager/shipment_manager/settings.py
@@ -31,7 +31,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'customers',
     'rest_framework',
-    'localflavor'
+    'localflavor',
+    'client'
 ]
 
 MIDDLEWARE = [
--- shipment_manager/shipment_manager/urls.py
+++ shipment_manager/shipment_manager/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('client.urls')),
     path('', include('customers.urls')),
     path('admin/', admin.site.urls),
 ]
